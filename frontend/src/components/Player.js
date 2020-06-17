5cbb9ab8693ba84ac3f5d71326a7ca68408cacc4
Sean Ye <seanye24@gmail.com>
2020-06-17 14:35:48 -0700

    Set basic layout of Player

--- frontend/src/components/Player.js
+++ frontend/src/components/Player.js
@@ -1,16 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import Opponent from './Opponent';
+import Hand from './Hand'
 
-const Player = () => {
-  const [cardToPlay, setCardToPlay] = useState(null)
-  const [turn, setTurn] = useState(0)
+const Player = (props) => {
+  const cards = props.cards;
+  const playersHands = props.playersHands;
+  const cardOnTop = props.cardOnTop;
+  const id = props.id;
+  const currTurn = props.currTurn;
+  const myTurn = id == currTurn;
 
-
+  const validateCard = (card) => {
+    return card.value === cardOnTop.value || card.color === cardOnTop.color;
+  };
 
   return (
     <div>
-
+      {playersHands.map((p) => (
+        <Opponent id={p.id} cards={p.cards} />
+      ))}
+      <Hand cards={cards} validateCard={validateCard} />
     </div>
-  )
-}
+  );
+};
 
 export default Player;
