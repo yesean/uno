803b0fe87aa13db9d06861a166e1e85dedb3cc79
Sean Ye <seanye24@gmail.com>
2020-06-16 18:30:33 -0700

    Move Components to src/Components

---
+++ frontend/src/components/Player.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+const Player = () => {
+  const [cardToPlay, setCardToPlay] = useState(null)
+  const [turn, setTurn] = useState(0)
+
+
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Player;
