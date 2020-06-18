db3933fd1155f331a3d39bccf3b8d8ebaafcab35
William Shue <williamshue0@gmail.com>
2020-06-18 01:34:58 -0700

    working colors

---
+++ backend/build/precache-manifest.931c7184f8e4e6a05a22f2223b442825.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df934fa0fd1bfbf8acb2e77aca435de7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e2f963c90039366b542",
+    "url": "/static/css/main.0f6f12cd.chunk.css"
+  },
+  {
+    "revision": "cf83944e934823c9abf2",
+    "url": "/static/js/2.268e7049.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.268e7049.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e2f963c90039366b542",
+    "url": "/static/js/main.47c29c2a.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
--- frontend/src/components/Game.js
+++ frontend/src/components/Game.js
@@ -1,21 +1,28 @@
-import React, { useState } from "react";
-import "./../App.css";
-import socketService from "./../services/socket.js";
-import Deck from "./Deck";
-import Opponent from "./Opponent";
-import Player from "./Player";
+import React, { useState } from 'react';
+import './../App.css';
+import socketService from './../services/socket.js';
+import Deck from './Deck';
+import Opponent from './Opponent';
+import Player from './Player';
 
 const Game = () => {
+  // const [id, setId] = useState(null);
+  // const [hand, setHand] = useState([]);
+  // const [cardOnTop, setCardOnTop] = useState([]);
+  // const [opponents, setOpponents] = useState([]);
+  // const [currTurn, setCurrTurn] = useState(null);
+  // const [winner, setWinner] = useState(null);
+
   const[props, setProps] = useState({
     id: null,
-    hand: [],
-    cardOnTop: [],
+    hand: [], 
+    cardOnTop: [], 
     opponents: [],
     currTurn: [],
     winner: [],
-  });
-
-  const { id, hand, cardOnTop, opponents, currTurn, winner } = props;
+  })
+  
+  const {id, hand, cardOnTop, opponents, currTurn, winner} = props
 
   const draw = () => {
     console.log(`calling draw`);
@@ -27,59 +34,69 @@ const Game = () => {
   };
 
   if (id) {
-    socketService.socket.on("fetch", (data) => {
+    socketService.socket.on('fetch', (data) => {
       console.log(`receiving data as player id ${id}`);
       console.log(`player data: ${data.playerData.join()}`);
       console.log('hand: ' + data.playerData.find((p) => p.id === id).hand);
+      // setWinner(data.winner);
+      // console.log(`rendering hand`)
+      // setCardOnTop(data.topCard);
+      // console.log(`rendering hand`)
+      // setCurrTurn(data.currPlayer);
+      // console.log(`rendering hand`)
+      // setHand(data.playerData.find((p) => p.id === id).hand);
+      // console.log(`rendering hand`)
+      // setOpponents(data.playerData.filter((p) => p.id !== id));
+      // console.log(`rendering hand`)
 
       setProps({
         ...props,
         hand: data.playerData.find((p) => p.id === id).hand,
         cardOnTop: data.topCard,
-        opponents: data.playerData.filter((p) => p.id !== id),
+        opponents:  data.playerData.filter((p) => p.id !== id),
         currTurn: data.currPlayer,
-        winner: data.winner,
-      });
-      if (!winner) {
-        return (
-          //players in circle
-          //deck in middle/ uno button
-          <div>
-            <center>
-              <table>
-                <tbody>
-                  <tr>
-                    {opponents.map((o) => (
-                      <td key={o.id} className="opponent">
-                        <Opponent opponent={o} />
-                      </td>
-                    ))}
-                  </tr>
-                </tbody>
-              </table>
-              <Deck />
-              <p>Top Card: {`${cardOnTop.color}${cardOnTop.value}`}</p>
-              <button onClick={() => draw()}>Draw</button>
-              <Player
-                player={{ hand: hand, id: id }}
-                cardOnTop={cardOnTop}
-                currTurn={currTurn}
-                playCard={playCard}
-              />
-            </center>
-          </div>
-          //player's deck with navigation buttons
-        );
-      } else {
-        return <h1>Game Over Boiz!</h1>;
-      }
+        winner: data.winner
+      })
     });
   }
 
   socketService.socket.on("giveID", (data) => {
-    setProps({ ...props, id: data.id });
+    setProps({...props, id: data.id});
   });
-  return null;
+
+  if (!winner) {
+    return (
+      //players in circle
+      //deck in middle/ uno button
+      <div>
+        <center>
+          <table>
+            <tbody>
+              <tr>
+                {opponents.map((o) => (
+                  <td key={o.id} className="opponent">
+                    <Opponent opponent={o} />
+                  </td>
+                ))}
+              </tr>
+            </tbody>
+          </table>
+          <Deck />
+          <p>Top Card: {`${cardOnTop.color}${cardOnTop.value}`}</p>
+          <button onClick={() => draw()}>Draw</button>
+          <Player
+            player={{ hand: hand, id: id }}
+            cardOnTop={cardOnTop}
+            currTurn={currTurn}
+            playCard={playCard}
+          />
+        </center>
+      </div>
+      //player's deck with navigation buttons
+    );
+  } else {
+    return <h1>Game Over Boiz!</h1>;
+  }
 };
 
 export default Game;
